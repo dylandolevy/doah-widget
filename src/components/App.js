f895d82b853ff5f269dcab650793b7d484b1f1b9
Dylan Dole-Vy <dylan.dole-vy@duke.edu>
2026-02-13 14:01:53 -0500

    Add initial implementation of App and MyChat components with styling

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { MyChat } from './components/MyChat';
+
+function App() {
+  return (
+    <div className="App">
+      <MyChat />
+    </div>
+  );
+}
+
+export default App;
